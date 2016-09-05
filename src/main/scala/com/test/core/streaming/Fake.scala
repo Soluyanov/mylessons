fb0218e3cdd308a0260d56daf51760efd99f917b
Alexander Soluyanov <soluyanov.a.v@gmail.com>
2016-09-05 14:15:10 +0300

    format

--- src/main/scala/com/test/core/streaming/Fake.scala
+++ src/main/scala/com/test/core/streaming/Fake.scala
@@ -1,13 +1,112 @@
 package com.test.core.streaming
 
-import org.apache.log4j.Logger
+import org.apache.hadoop.io.SequenceFile.CompressionType
+import org.apache.hadoop.io.compress.CompressionCodec
+import org.apache.spark._
 import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.io.NullWritable
+import org.apache.log4j.Logger
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
+import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 import org.apache.spark.streaming.Milliseconds
-import org.apache.spark.streaming.StreamingContext
-
-
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream._
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
+import org.apache.hadoop.io.{
+  ArrayWritable,
+  BooleanWritable,
+  BytesWritable,
+  DoubleWritable,
+  FloatWritable,
+  IntWritable,
+  LongWritable,
+  NullWritable,
+  Text,
+  Writable
+}
+import org.apache.spark.rdd.PairRDDFunctions
+import scala.collection.mutable.ListBuffer
+import org.apache.hadoop.io.compress.GzipCodec
+import java.io.PrintWriter
+import org.apache.hadoop.fs.FSDataOutputStream
+import java.io._
 object Fake {
-  def fakecalc(): Double = 1.0
+
+  val Log = Logger.getLogger(Fake.this.getClass().getSimpleName())
+
+  def main(args: Array[String]) {
+    if (args.length < 4) {
+      System.err.println(
+        "Usage: KafkaWordCountProducer <metadataBrokerList> <topic> " +
+          "<messagesPerSec> <wordsPerMessage>")
+      System.exit(1)
+    }
+
+    val Array(zkQuorum, group, topics, numThreads) = args
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("Fake")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val counts =
+      KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+    counts.print()
+
+    val hadoopConf = new org.apache.hadoop.conf.Configuration()
+    val prefix =
+      "hdfs://c6402.ambari.apache.org:8020/user/hdfs/newdir/output23.txt"
+    val path = new org.apache.hadoop.fs.Path(prefix)
+    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+    counts.foreachRDD(
+      rdd =>
+        rdd
+          .collect()
+          .foreach(line =>
+            (
+              if (hdfs.exists(path) == true) { //проверяем, существует ли уже файл, в который собираемся писать
+                val fsDataOutputStream = hdfs.append(path) //создаём исходящий поток, добавляющий записи к существующему файлу
+                val outputStreamWriter = new OutputStreamWriter(
+                  fsDataOutputStream) //создаём то, что будет писать поток
+                val bufferedWriter = new BufferedWriter(outputStreamWriter) //буферизируем записываемые данные чтобы снизить количество обращений к физическому носителю. Можно и не делать этого.
+                bufferedWriter.write(line) //записываем данные в файл
+                bufferedWriter.close()
+                outputStreamWriter.close()
+                fsDataOutputStream.close()
+              } else { //здесь всё то же самое, только сначала файл создаётся
+                val fsDataOutputStream = hdfs.create(path)
+                val outputStreamWriter =
+                  new OutputStreamWriter(fsDataOutputStream)
+                val bufferedWriter = new BufferedWriter(outputStreamWriter)
+                bufferedWriter.write(line)
+                bufferedWriter.close()
+                outputStreamWriter.close()
+                fsDataOutputStream.close()
+              }
+          )))
+
+    Log.error("DEBUG info:" + zkQuorum)
+
+    sys.ShutdownHookThread({
+      println("Ctrl+C")
+      try {
+        hdfs.close()
+        ssc.stop(stopSparkContext = true, stopGracefully = true)
+      } catch {
+        case e: Throwable => {
+          println("exception on ssc.stop(true, true) occured")
+        }
+      }
+    })
+
+    ssc.start()
+
+    ssc.awaitTermination()
+
+  }
 }
